4adfe43c825df81ad916a0e4d9384c141603126f
yuri <yamayuri@gmail.com>
2013-12-08 18:46:43 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   has_secure_password
 #  before_save { email.downcase! }
   before_save { self.email = email.downcase }
@@ -18,10 +19,15 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
+
   private
 
-  def create_remember_token
-    self.remember_token = User.encrypt(User.new_remember_token)
-  end
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -89,9 +89,21 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
-
-
       end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
     end
 
     describe "as wrong user" do
